7616cc394e0c04fabb2e834c7d5d129b1d441445
Dieter <vanderdt@ucmail.uc.edu>
2021-02-04 12:45:04 -0500

    Debugging

--- main_MB.py
+++ main_MB.py
@@ -29,33 +29,41 @@ trigger_pin2 = pyb.Pin('X2', pyb.Pin.OUT_PP)
 trigger_pin1.low()
 trigger_pin2.low()
 
+
+test = False
+
 def measure(channel, fs, duration):
     value = 0
     samples = int((fs/1000) * duration)
     timer = pyb.Timer(6, freq=fs)
     buffer = array.array('H', (0 for i in range(samples)))
+    if test: print('Channel', channel)
     
-    if channel == 1:
-        trigger_pin1.high()
-        utime.sleep_us(30)
-        trigger_pin1.low()        
-        while value < signal_threshold: value = adc1.read()
-        adc1.read_timed(buffer, timer)
+    if channel == 1: trigger_pin1.high()
+    if channel == 2: trigger_pin2.high()
     
-    time.sleep(0.1)
-        
-    if channel == 2:
-        trigger_pin2.high()
-        utime.sleep_us(30)
-        trigger_pin2.low()
-        while value < signal_threshold: value = adc2.read()
-        adc2.read_timed(buffer, timer)
-        
+    utime.sleep_us(50)
+    
+    trigger_pin1.low()
+    trigger_pin2.low()
+    
+    start_counter = utime.ticks_ms()
+    while value < signal_threshold:
+        if channel == 1: value = adc1.read()
+        if channel == 2: value = adc2.read()
+        current_counter = utime.ticks_ms()
+        if current_counter - start_counter > 1000: break
+
+    
+    if channel == 1: adc1.read_timed(buffer, timer)
+    if channel == 2: adc2.read_timed(buffer, timer) 
+    
     return buffer
 
 
 def measure_both(first, second, fs, duration):
     buffer1 = measure(first, fs, duration)
+    utime.sleep_ms(100)
     buffer2 = measure(second, fs, duration)
     total = buffer1 + buffer2
     return total
@@ -78,29 +86,30 @@ def listen_for_uart():
     return parameters
 
 
-# while True:
-#     buffer = measure_both(1, 2, 10000, 10)
-#     utime.sleep(1)
-#     print('done')
-
-# a = utime.ticks_ms()
-# buffer = measure_both(1, 2, 10000, 10)
-# b = utime.ticks_ms()
-# print(b-a)
 
 
-blue.off()
-while True:
-    parameters = listen_for_uart()
-    blue.on()
-    first_channel = parameters[0]
-    second_channel = parameters[1]
-    sample_rate = parameters[2]
-    duration = parameters[3]
-    total_buffer = measure_both(first_channel, second_channel, sample_rate, duration)
-    uart.write(total_buffer)
+if test:
+    while True:
+        buffer = measure_both(1, 2, 10000, 10)
+        utime.sleep(0.1)
+        print('done')
+else:
     blue.off()
-    
+    while True:
+        parameters = listen_for_uart()
+        blue.on()
+        first_channel = parameters[0]
+        second_channel = parameters[1]
+        sample_rate = parameters[2]
+        duration = parameters[3]
+        green.on()
+        total_buffer = measure_both(first_channel, second_channel, sample_rate, duration)
+        green.off()
+        orange.on()
+        uart.write(total_buffer)
+        orange.off()
+        blue.off()
+        
 
 
        
